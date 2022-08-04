f89d357269177a408868aee168979c5355034678
Hennadii Zykov <hennadii.zykov@gmail.com>
2022-08-03 20:07:28 -0400

    New files

---
+++ src/test/java/tiger/api/test/data/DataGenerator.java
@@ -0,0 +1,47 @@
+package tiger.api.test.data;
+
+import com.github.javafaker.Faker;
+
+public class DataGenerator {
+// Gena Zykov
+	public static String getEmail() {
+		Faker faker = new Faker();
+		return faker.name().firstName() + faker.name().lastName() + "@gmail.com";
+
+	}
+// Change get method phoneNumber
+	public static String getPhoneNumber() {
+		Faker faker = new Faker();
+		return faker.phoneNumber().cellPhone();
+
+	}
+ // Change method getFirstName
+	public static String getFirstName() {
+		Faker faker = new Faker();
+		return faker.name().firstName();
+				
+	}
+	//change method getLastName
+	public static String getLastName() {
+		Faker faker = new Faker();
+		return faker.name().lastName();
+		
+				
+	}
+	// Change method getDob
+	public static String getDob() {
+		Faker faker = new Faker();
+	
+		return faker.date().birthday().toString();
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(getEmail());
+		System.out.println(getFirstName());
+		System.out.println(getLastName());
+	}
+	
+	
+	
+}
